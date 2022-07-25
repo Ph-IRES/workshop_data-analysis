165c510ab691461b3cff693a885af485e32ef5eb
cbird808 <cbird808@gmail.com>
2022-07-25 05:40:24 -0500

    update

--- statistics/biodiversity/README.md
+++ statistics/biodiversity/README.md
@@ -50,7 +50,7 @@
 
 2. Wrangle data into format compatible with vegan.
 
-	Each column is a site and each row is a unique taxon
+	For the species count data, each column is a taxon and each row is a unique site or observation
 
 	We will use the count data from the `salvador` repo which is already here in this directory.  
 	
@@ -72,11 +72,47 @@
 	  summarize(sum_max_n = sum(max_n)) %>%
 	  ungroup() %>%
 	  # convert tibble from long to wide format
-	  pivot_wider(names_from = op_code,
+	  pivot_wider(names_from = taxon,
 				  values_from = sum_max_n,
 				  values_fill = 0) %>%
-	  # remove the taxon column for vegan
-	  dplyr::select(-taxon)
+	  # sort by op_code
+	  arrange(op_code) %>%
+	  # remove the op_code column for vegan
+	  dplyr::select(-op_code)
+	```
+
+	For the metadata, each row must be in same order as in data tibble above:
+	
+	```r
+	metadata_vegan <-
+	  data_all %>%
+	  # make unique taxa
+	  mutate(taxon = str_c(family,
+						   genus,
+						   species,
+						   sep = "_")) %>%
+	  # sum all max_n counts for a taxon and op_code
+	  group_by(taxon,
+			   op_code,
+			   site,
+			   survey_area,
+			   habitat,
+			   lat_n,
+			   long_e,
+			   depth_m,
+			   bait_type) %>%
+	  summarize(sum_max_n = sum(max_n)) %>%
+	  ungroup() %>%
+	  # convert tibble from long to wide format
+	  pivot_wider(names_from = taxon,
+				  values_from = sum_max_n,
+				  values_fill = 0) %>%
+	  # sort by op_code
+	  arrange(op_code) %>%
+	  # remove the op_code column for vegan
+	  dplyr::select(op_code:bait_type) %>%
+	  mutate(site_code = str_remove(op_code,
+									"_.*$"))
 	```
 
 ## Ordination
@@ -92,28 +128,53 @@ Note that `vegan` is not `tidyverse` compatible, meaning that its functions are 
 	ord <- decorana(data_vegan)
 	ord
 	summary(ord)
+	#boring plot
 	plot(ord)
-	
-	#nicer plot
-	plot(ord, type = "n")
-	points(ord, display = "sites", cex = 0.8, pch=21, col="red", bg="yellow")
-	text(ord, display = "spec", cex=0.7, col="blue")
 	```
 	
-![](Rplot.png)
+!{}(Rplot.png)
+The symbols represent the sites (black circles) and the taxa (red +)
 	
+	```r
+	#fancier plot
+	plot(ord, type = "n")
+	points(ord, display = "sites", cex = 0.8, pch=21, col="black", bg="yellow")
+	text(ord, display = "spec", cex=0.7, col="red")
+	```
+	
+![](Rplot01.png)
+The sites are black circles and the taxa are spelled out in red.
+	
+	```r
+	#fanciest plot
+	plot(ord, disp="sites", type="n")
+	ordihull(ord, habitat, col=1:2, lwd=3)
+	ordiellipse(ord, habitat, col=1:2, kind = "ehull", lwd=3)
+	ordiellipse(ord, habitat, col=1:2, draw="polygon")
+	points(ord, disp="sites", pch=21, col=1:2, bg="yellow", cex=1.3)
+	ordispider(ord, habitat, col=1:2, label = TRUE)
+	```
+	
+![](Rplot02.png)
+Color coded by habitat, where deep reef is black and shallow reef is red.
+
+---
+
 	```r
 	# Non-metric multidimensional scaling
 	ord <- metaMDS(data_vegan)
 	ord
 	summary(ord)
-	plot(ord)
-	plot(ord, type = "n")
-	points(ord, display = "sites", cex = 0.8, pch=21, col="red", bg="yellow")
-	text(ord, display = "spec", cex=0.7, col="blue")
+	#fanciest plot
+	plot(ord, disp="sites", type="n")
+	ordihull(ord, habitat, col=1:2, lwd=3)
+	ordiellipse(ord, habitat, col=1:2, kind = "ehull", lwd=3)
+	ordiellipse(ord, habitat, col=1:2, draw="polygon")
+	points(ord, disp="sites", pch=21, col=1:2, bg="yellow", cex=1.3)
+	ordispider(ord, habitat, col=1:2, label = TRUE)
 	```
 	
-![](Rplot01.png)
+![](Rplot03.png)
 
 
 
--- statistics/biodiversity/data_wrangling_vis_salvador.R
+++ statistics/biodiversity/data_wrangling_vis_salvador.R
@@ -28,13 +28,21 @@ data <-
   # there is a different depth for CAG_024 in data vs metadata
   # solution: go with metadata depth, confirm with Gene & Rene
   mutate(depth_m = case_when(op_code == "CAG_024" ~ 8,
-                             TRUE ~ depth_m))
+                             TRUE ~ depth_m),
+         family = str_to_title(family),
+         genus = str_to_title(genus),
+         species = str_to_lower(species),
+         trophic_groups = str_to_title(trophic_groups))
 
 metadata <-
   read_excel(inFilePath2,
              na="NA") %>%
   clean_names() %>%
-  dplyr::rename(bait_weight_grams = weight_grams)
+  dplyr::rename(bait_weight_grams = weight_grams) %>%
+  mutate(site = str_to_title(site),
+         survey_area = str_to_title(survey_area),
+         habitat = str_to_title(habitat),
+         bait_type = str_to_title(bait_type))
 
 #### COMBINE DATA ####
 
@@ -50,133 +58,14 @@ data_all <-
          time_in:bait_weight_grams,
          everything())
 
-#### VISuALIZE METADATA ####
-
-metadata %>%
-  ggplot(aes(x=depth_m,
-             fill = habitat)) +
-  geom_histogram() +
-  theme_classic() 
-ggsave("histogram_depth-x-habitat.png")
-
-metadata %>%
-  ggplot(aes(x=depth_m,
-             fill = habitat)) +
-  geom_histogram() +
-  theme_classic() +
-  facet_grid(bait_type ~ bait_weight_grams)
-ggsave("histogram_depth-x-habitat-x-bait.png")
-
-metadata %>%
-  ggplot(aes(x=habitat,
-             y=survey_length_hrs,
-             fill = habitat)) +
-  # geom_violin() +
-  geom_boxplot() +
-  theme_classic() 
-
-metadata %>%
-  ggplot(aes(y=lat_n,
-             x=long_e,
-             color = habitat)) +
-  geom_point(size = 5) +
-  theme_classic() 
-
-#### VISUALIZE ALL DATA ####
-
-data_all %>%
-  ggplot(aes(x=habitat,
-             y=max_n,
-             color = habitat)) +
-  geom_boxplot() +
-  theme_classic() +
-  facet_grid(trophic_groups ~ .)
-
-data_all %>%
-  ggplot(aes(x=max_n,
-             fill = habitat)) +
-  geom_histogram(position="identity") +
-  theme_classic() +
-  facet_grid(. ~ trophic_groups,
-             scales = "free_x")
-
-#### MAP DATA ####
-#https://www.datanovia.com/en/blog/how-to-create-a-map-using-ggplot2/
-
-plot1 <- 
-  metadata %>%
-    ggplot(aes(y=lat_n,
-               x=long_e,
-               color = habitat)) +
-    geom_point(size = 5) +
-    theme_classic()
-
-world_map <- map_data("world")
-
-world_map %>%
-ggplot(aes(x = long, 
-           y = lat, 
-           group = group)) +
-  geom_polygon(fill="lightgray", 
-               color = "red")
-
-map_data("world",
-         region = "Philippines") %>%
-  ggplot(aes(x = long, 
-             y = lat,
-             group = group)) +
-  geom_polygon(fill="lightgray",
-               colour = "black") 
-
-subregion_label_data <- 
-  map_data("world",
-           region = "Philippines") %>%
-  dplyr::group_by(subregion,
-                  group) %>%
-  dplyr::summarize(long = mean(long), 
-                   lat = mean(lat)) %>%
-  filter(subregion == "Negros" |
-           subregion == "Cebu")
-
-region_label_data <- 
-  map_data("world",
-           region = "Philippines") %>%
-  dplyr::group_by(region) %>%
-  dplyr::summarize(long = mean(long), 
-                   lat = mean(lat))
-
-map_data("world",
-         region = "Philippines") %>%
-  ggplot(aes(long,
-             lat,
-             group=group)) +
-  geom_polygon(fill="lightgray",
-               color = "black") +
-  # geom_text(data = subregion_label_data,
-  #           aes(label = subregion),
-  #           size = 6,
-  #           hjust = 0.5) +
-  geom_text(data = region_label_data,
-            aes(x = long,
-                y= lat,
-                label = region),
-            size = 10,
-            hjust = 0.5,
-            inherit.aes = FALSE) +
-  geom_point(data = metadata,
-             aes(x = long_e,
-                 y = lat_n,
-                 color = habitat),
-             inherit.aes = FALSE)
-
-
 #### DIVERSITY ####
 
-# convert data into tibble for vegan ingestion
+# convert species count data into tibble for vegan ingestion
   # each row is a site
   # each column is a taxon
+  # data are counts
 
-data_biodiv <-
+data_vegan <-
   data %>%
   # make unique taxa
   mutate(taxon = str_c(family,
@@ -187,13 +76,79 @@ data_biodiv <-
   group_by(taxon,
            op_code) %>%
   summarize(sum_max_n = sum(max_n)) %>%
+  ungroup() %>%
+  # convert tibble from long to wide format
   pivot_wider(names_from = taxon,
               values_from = sum_max_n,
-              values_fill = 0)
+              values_fill = 0) %>%
+  # sort by op_code
+  arrange(op_code) %>%
+  # remove the op_code column for vegan
+  dplyr::select(-op_code)
 
-data_vegan <-
-  data_biodiv %>%
-  select(-op_code)
+# convert metadata into tibble for vegan ingestion
+# each row is a site
+# each column is a dimension of site, such as depth, lat, long, region, etc
 
+data_vegan.env <-
+  data_all %>%
+  # make unique taxa
+  mutate(taxon = str_c(family,
+                       genus,
+                       species,
+                       sep = "_")) %>%
+  # sum all max_n counts for a taxon and op_code
+  group_by(taxon,
+           op_code,
+           site,
+           survey_area,
+           habitat,
+           lat_n,
+           long_e,
+           depth_m,
+           bait_type) %>%
+  summarize(sum_max_n = sum(max_n)) %>%
+  ungroup() %>%
+  # convert tibble from long to wide format
+  pivot_wider(names_from = taxon,
+              values_from = sum_max_n,
+              values_fill = 0) %>%
+  # sort by op_code
+  arrange(op_code) %>%
+  # remove the op_code column for vegan
+  dplyr::select(op_code:bait_type) %>%
+  mutate(site_code = str_remove(op_code,
+                                "_.*$"),
+         site_code = factor(site_code),
+         habitat = factor(habitat))
+
+# and now we "attach" the metadata to the data
+
+attach(data_vegan.env)
+
+# Detrended correspondence analysis
 ord <- decorana(data_vegan)
 ord
+summary(ord)
+#boring plot
+plot(ord)
+#fancier plot
+plot(ord, type = "n")
+points(ord, display = "sites", cex = 0.8, pch=21, col="black", bg="yellow")
+text(ord, display = "spec", cex=0.7, col="red")
+#fanciest plot
+plot(ord, disp="sites", type="n")
+ordihull(ord, habitat, col=1:2, lwd=3)
+ordiellipse(ord, habitat, col=1:2, kind = "ehull", lwd=3)
+ordiellipse(ord, habitat, col=1:2, draw="polygon")
+points(ord, disp="sites", pch=21, col=1:2, bg="yellow", cex=1.3)
+ordispider(ord, habitat, col=1:2, label = TRUE)
+
+# Non-metric multidimensional scaling
+ord <- metaMDS(data_vegan)
+ord
+summary(ord)
+plot(ord)
+plot(ord, type = "n")
+points(ord, display = "sites", cex = 0.8, pch=21, col="red", bg="yellow")
+text(ord, display = "spec", cex=0.7, col="blue")
